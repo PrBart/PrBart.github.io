e8c2f1a6bc58bb1756dd41d224634b142626139f
PrBart <d.samovilov@gmail.com>
2025-05-07 14:35:01 +0200

    Added 404 vite workaround

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import fs from 'fs'
-
-function spa404Plugin() {
-  return {
-    name: 'spa-404-plugin',
-    closeBundle() {
-      const indexPath = path.resolve(__dirname, 'dist/index.html')
-      const notFoundPath = path.resolve(__dirname, 'dist/404.html')
-      fs.copyFileSync(indexPath, notFoundPath)
-    }
-  }
-}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), spa404Plugin()],
+  plugins: [react()],
 })
