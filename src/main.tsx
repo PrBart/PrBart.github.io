d543b4cbaae653201832f1b4f2ed3d46d8f7e07d
PrBart <d.samovilov@gmail.com>
2025-05-29 20:50:59 +0400

    Revert "Style download fix"
    This reverts commit 19564d77282170c44119b71e4be5a1e7c88d26fa.

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { loadStyles } from "./lib/styleLoader";
 
 import "./index.css";
 
 const theme = import.meta.env.VITE_CV_THEME;
 
-const loadThemeStyles = async () => {
-  const styles: string[] = [];
+switch (theme) {
+  case "retro":
+    import("./themes/retro.css");
+    break;
+  case "screen":
+    import("./themes/screen.css");
+    break;
+  case "github":
+  default:
+    import("github-markdown-css/github-markdown.css");
+    import("./themes/github-markdown-override.css");
+    break;
+}
 
-  switch (theme) {
-    case "retro":
-      styles.push("/src/themes/retro.css");
-      break;
-    case "screen":
-      styles.push("/src/themes/screen.css");
-      break;
-    case "github":
-    default:
-      styles.push("/node_modules/github-markdown-css/github-markdown.css");
-      styles.push("/src/themes/github-markdown-override.css");
-      break;
-  }
-
-  await loadStyles(styles);
-};
-
-const init = async () => {
-  try {
-    await loadThemeStyles();
-    
-    const root = createRoot(document.getElementById("root")!);
-    root.render(
-      <StrictMode>
-        <App />
-      </StrictMode>
-    );
-  } catch (error) {
-    console.error("Failed to load styles:", error);
-    // You might want to show an error message to the user here
-  }
-};
-
-init();
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
