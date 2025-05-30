d889de99b85938e1f451efccbe7cf98b886dcebd
PrBart <d.samovilov@gmail.com>
2025-05-30 14:36:43 +0400

    Added manual and on commit workflows, added spinner, small code improvements

--- src/MarkdownPage.tsx
+++ src/MarkdownPage.tsx
@@ -1,8 +1,10 @@
+import { useMemo } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Navigate, useParams } from "react-router-dom";
 
 import LanguageSwitcher from "./LanguageSwitcher";
+import { LoadingSpinner } from "./components/LoadingSpinner";
 import { PrintButtonLanguage } from "./consts";
 import {
   defaultLang,
@@ -19,42 +21,49 @@ type Props = {
 
 export default function MarkdownPage({ isDefaultLang }: Props) {
   const { lang } = useParams<{ lang: string }>();
-
   const currentLang = isDefaultLang ? defaultLang : lang ?? defaultLang;
+
+  const markdown = useMemo(() => getMarkdown(currentLang), [currentLang]);
+  
+  const printLabelText = useMemo(() => 
+    PrintButtonLanguage[currentLang as keyof typeof PrintButtonLanguage] ||
+    PrintButtonLanguage.en,
+    [currentLang]
+  );
 
   if (lang && !supportedLanguages.includes(lang)) {
     return <Navigate to="/" replace />;
   }
-  const markdown = getMarkdown(currentLang);
-  const printLabelText =
-    PrintButtonLanguage[currentLang as keyof typeof PrintButtonLanguage] ||
-    PrintButtonLanguage.en;
+
+  if (!markdown) {
+    return <LoadingSpinner />;
+  }
 
   return (
-    <div className="page-conteiner">
-      <div className="markdown-body markdown-page">
-        {/* Language selector + Print button */}
-        <div className="top-buttons-conteiner no-print">
-          <div className="language-selector">
-            {!(supportedLanguages.length < 2) && (
-              <LanguageSwitcher currentLang={currentLang} />
-            )}
-            <div className="print-button-container">
-              <button
-                className="print-button no-print"
-                onClick={() => window.print()}
-              >
-                {printLabelText}
-              </button>
+    <div className="page-container">
+        <div className="markdown-body markdown-page">
+          {/* Language selector + Print button */}
+          <div className="top-buttons-container no-print">
+            <div className="language-selector">
+              {supportedLanguages.length > 1 && (
+                <LanguageSwitcher currentLang={currentLang} />
+              )}
+              <div className="print-button-container">
+                <button
+                  className="print-button no-print"
+                  onClick={() => window.print()}
+                >
+                  {printLabelText}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Markdown content */}
-        <div className="markdown-body">
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+          {/* Markdown content */}
+          <div className="markdown-body">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+          </div>
         </div>
-      </div>
     </div>
   );
 }
